6b02e732dd6e33a8bfa10dde1e313a6df25f0a39
Rajasekhar <rajalekkalaa@gmail.com>
2021-07-31 11:13:28 +0800

    Add Reconnecting Websocket lib and server status indicator

--- chat-application/cmd/web/routes.go
+++ chat-application/cmd/web/routes.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/lekkalraja/go-by-websockets/chat-application/handlers"
 )
@@ -9,5 +11,7 @@ func routes() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/", handlers.Home)
 	r.HandleFunc("/ws", handlers.WsEndpoint)
+	fileServer := http.FileServer(http.Dir("./static/"))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static", fileServer))
 	return r
 }
